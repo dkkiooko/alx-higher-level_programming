9b130bab410421464926c622233f4c8b8ca6ecde
dkkiooko <kithekadankioko@gmail.com>
2022-09-20 10:03:24 +0300

    more corrections

--- 0x00-python-hello_world/6-concat.py
+++ 0x00-python-hello_world/6-concat.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 str1 = "Holberton"
 str2 = "School"
-str1 = f"{str1} {str2}\n"
+str1 = f"{str1:s} {str2:s}\n"
 print(f"Welcome to {str1}!")
--- 0x00-python-hello_world/8-concat_edges.py
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 str = "Python is an interpreted, interactive, object-oriented programming\
  language that combines remarkable power with very clear syntax"
-str = str[39:66] + str[106:112] + str[:6] + '\n'
+str = f"{str[39:66]:s}{str[106:112]:s}{str[:6]:s}\n"
 print(str)
