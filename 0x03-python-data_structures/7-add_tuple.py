b3b3ca4e0b33176257d87cf08f86501371f6ad8d
dkkiooko <kithekadankioko@gmail.com>
2022-09-25 14:03:22 +0300

    reassigning a tuple in place using the + operator and indexing using []

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
-    if (len(tuple_a) == 2) and (len(tuple_b) == 2):
-        result = tuple(a+b for a,b in zip(tuple_a, tuple_b))
-        return result
-    
+    tuple_a, tuple_b = tuple_a + (0,0), tuple_b + (0,0)
+    result = (tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1])
+    return result
