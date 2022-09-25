bb5dc6b105edfa76e0245bfca1b840ca62089d75
dkkiooko <kithekadankioko@gmail.com>
2022-09-25 13:32:04 +0300

    replaced the print and actually returned a string

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -4,4 +4,4 @@ def no_c(my_string):
     for i in range(len(my_string)):
         if my_string[i] == 'c' or my_string[i] == 'C':
             my_string[i] = ''
-    print(''.join(my_string))
+    return ''.join(my_string)
