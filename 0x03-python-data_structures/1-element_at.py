205be6bcf0a13dbfae3f92f41aaa6573f9c6d463
dkkiooko <kithekadankioko@gmail.com>
2022-09-25 13:14:40 +0300

    removed unneccessary else

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -2,6 +2,4 @@
 def element_at(my_list, idx):
     if (idx < 0) or (idx >= len(my_list)):
         return None
-    else:
-        return my_list[idx]
-print(element_at([23,4,5,6,7],-5))
+    return my_list[idx]
