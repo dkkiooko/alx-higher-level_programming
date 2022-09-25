e377d4fd59a8a8deed410dff878801e903cddab8
dkkiooko <kithekadankioko@gmail.com>
2022-09-25 13:51:16 +0300

    correct a pycodestyle error

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -3,7 +3,7 @@ def print_matrix_integer(matrix=[[]]):
     if matrix:
         for element in matrix:
             for i in range(len(element)):
-                if i ==  len(element) - 1:
+                if i == len(element) - 1:
                     print("{:d}".format(element[i]), end="")
                 else:
                     print("{:d}".format(element[i]), end=" ")
