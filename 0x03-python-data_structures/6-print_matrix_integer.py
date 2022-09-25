3e7c37ca16a1c69845bacbc3e6529bd2d496da78
dkkiooko <kithekadankioko@gmail.com>
2022-09-25 13:40:00 +0300

    rearranged printing structure as well as the print function after every row

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,12 +1,10 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-    for element in matrix:
-        for elements in element:
-            print(f"{elements:d}", end=" ")
-        print('\n')
-matrix = [
-    [1,2,3],
-    [4,5,6],
-    [7,8,9]
-]
-print_matrix_integer(matrix)
+    if matrix:
+        for element in matrix:
+            for i in range(len(element)):
+                if i ==  len(element) - 1:
+                    print("{:d}".format(element[i]), end="")
+                else:
+                    print("{:d}".format(element[i]), end=" ")
+            print()
