946581f19af52fa3dc7bbdd2b863b0f7c8a835bf
dkkiooko <kithekadankioko@gmail.com>
2022-09-25 14:08:54 +0300

    changed code to fit all return cases

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -2,8 +2,8 @@
 def multiple_returns(sentence):
     length = len(sentence)
     if length == 0:
-        first = None
+        return (0, None)
     else:
         first = sentence[0]
-    result = tuple(length, first)
-    return result
+        result = (length, first)
+        return result
