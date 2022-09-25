ef5a50710328f8d5d1b76a45b312598920a0f222
dkkiooko <kithekadankioko@gmail.com>
2022-09-25 14:12:01 +0300

    fix speed issue

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -2,8 +2,8 @@
 def max_integer(my_list=[]):
     if len(my_list) == 0:
         return None
-    maximum = 0
+    maximum = my_list[0]
     for element in my_list:
-        if element >= maximum:
+        if element > maximum:
             maximum = element
     return maximum
