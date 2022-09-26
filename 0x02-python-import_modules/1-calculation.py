48b72aaa6185659dcc69cbdd3d35bf68c5c6df5d
dkkiooko <kithekadankioko@gmail.com>
2022-09-26 10:57:39 +0300

    edited the syntax in the function parameters to include an equal sign

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -6,7 +6,7 @@ from calculator_1 import div
 if __name__ == "__main__":
     a = 10
     b = 5
-    print('{:d} + {:d} = {:d}'.format(a, b, add(a, b)))
-    print('{:d} - {:d} = {:d}'.format(a, b, sub(a, b)))
-    print('{:d} * {:d} = {:d}'.format(a, b, mul(a, b)))
-    print('{:d} / {:d} = {:d}'.format(a, b, div(a, b)))
+    print('{:d} + {:d} = {:d}'.format(a, b, add(a=a, b=b)))
+    print('{:d} - {:d} = {:d}'.format(a, b, sub(a=a, b=b)))
+    print('{:d} * {:d} = {:d}'.format(a, b, mul(a=a, b=b)))
+    print('{:d} / {:d} = {:d}'.format(a, b, div(a=a, b=b)))
