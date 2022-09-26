6c9878d29e1619818e3941329a31148ab21a98de
dkkiooko <kithekadankioko@gmail.com>
2022-09-26 10:34:41 +0300

    code is not executed when imported

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
+from caluclator_1 import add
+from calculator_1 import sub
+from calculator_1 import mul
+from calculator_1 import div
 if __name__ == "__main__":
-    from caluclator_1 import add
-    from calculator_1 import sub
-    from calculator_1 import mul
-    from calculator_1 import div
     a = 10
     b = 5
     print('{:d} + {:d} = {:d}'.format(a, b, add(a, b)))
