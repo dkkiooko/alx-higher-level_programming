d8810fe99b9edb19953fd93e2636525a0a57323f
dkkiooko <kithekadankioko@gmail.com>
2022-10-12 13:20:05 +0300

    added an arguments section in the docstring

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,8 +1,11 @@
 #!/usr/bin/python3
 class Square:
-    """Empty class that defines a square
-    
+    '''Empty class that defines a square
+
+    Args:
+        None
+
     Attributes:
         None
-    """
+    '''
     pass
