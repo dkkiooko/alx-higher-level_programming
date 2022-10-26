2e7f5c2de7deb9d1a6e2229db393416f54848668
dkkiooko <kithekadankioko@gmail.com>
2022-10-26 11:48:32 +0300

    made a few corrections in the test files

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -25,6 +25,6 @@ class BaseGeometry:
             ValueError: _value must be larger than 0_
         """
         if type(value) != int:
-            raise TypeError("{} must be an integer".format(name))
+            raise TypeError("{:s} must be an integer".format(name))
         if value <= 0:
-            raise ValueError("{} must be greater than 0".format(name))
+            raise ValueError("{:s} must be greater than 0".format(name))
