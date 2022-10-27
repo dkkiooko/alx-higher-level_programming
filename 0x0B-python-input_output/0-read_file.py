ca87f553dd32a929b007d2e0036e67c43031f062
dkkiooko <kithekadankioko@gmail.com>
2022-10-27 05:34:11 +0300

    a few corrections

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -8,5 +8,6 @@ def read_file(filename=""):
     Args:
         filename (str, optional): _location of file on comp_. Defaults to "".
     """
-    with open(filename, 'r', encoding='utf-8') as f:
-        print(f.read())
+    with open(filename, "r", encoding="utf-8") as f:
+        for line in f.read():
+            print(line, end="")
