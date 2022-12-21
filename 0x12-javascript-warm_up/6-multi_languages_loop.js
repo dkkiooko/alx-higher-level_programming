49c80f9bb0197567437061b002ded0f88a27d075
dkkiooko <kithekadankioko@gmail.com>
2022-12-21 22:31:02 +0300

    fixed more spelling errors

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 // prints 3 lines using an array of string and loop
 
-const langs = ['C is fun', 'Python is cool', 'Javascript is amazing'];
+const langs = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
 
 for (let i = 0; i < 3; i++) {
   console.log(langs[i]);
