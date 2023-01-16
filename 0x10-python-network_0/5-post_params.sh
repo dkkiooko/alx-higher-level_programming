3683a83ee3f85707378663971ca811e0d7f59285
dkkiooko <kithekadankioko@gmail.com>
2023-01-16 17:52:45 +0300

    changed the order of the flags with -sX (silent rogue) coming before the POST method and -d (data) right before the variables

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 # sends POST request for the body of response with various variables
-curl -sd "email=test@gmail.com&subject=I%20wi
-ll%20always%20be%20here%20for%20PLD" -X POST "$1"
-
+curl "$1" -sX POST -d "email=test@gmail.com&subject=I%20wi
+ll%20always%20be%20here%20for%20PLD" 
