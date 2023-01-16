a279b2b7e34f1b4d8f02c0ee4de71afb434e8aca
dkkiooko <kithekadankioko@gmail.com>
2023-01-16 17:55:33 +0300

    replaced all "%20" with " "

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # sends POST request for the body of response with various variables
-curl "$1" -sX POST -d "email=test@gmail.com&subject=I%20wi
-ll%20always%20be%20here%20for%20PLD" 
+curl "$1" -sX POST -d "email=test@gmail.com&subject=I wi
+ll always be here for PLD" 
